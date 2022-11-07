3181cb065041da992692b020273e1b4ee6638250
cletacoco <cletacoco91@naver.com>
2022-11-07 12:54:15 +0900

    11-7

---
+++ day16/드래그.js
@@ -0,0 +1,11 @@
+
+const drag = document.getElementById('drag')
+
+/* 
+mousedown : 마우스 버튼을 눌렀을때
+mousemove : 마우스가 움직일때
+mouseup : 마우스 버튼을 뗐을때
+touchstart : 터치가 시작될때
+touchmove : 터치가 움직일때
+touchend : 터치가 끝났을때
+*/
---
+++ day18/practice/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Contents from './Content';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>my note</h1>
+
+      <Contents 제목="여기는제목1" 내용 ="여기는 내용1"/>
+      <Contents 제목="여기는제목2" 내용 ="여기는 내용2"/>
+      <Contents 제목="여기는제목3" 내용 ="여기는 내용3"/>
+      <Contents 제목="여기는제목4" 내용 ="여기는 내용4"/>
+
+    </div>
+  );
+}
+
+export default App;
